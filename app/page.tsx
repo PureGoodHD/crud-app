9441f3ca1c6c60df6ae5f4e6b1ef4f2c85293570
PureGoodHD <joshuaosena158@gmail.com>
2026-09-23 12:25:32 +0800

    Fix TypeScript error in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ async function getStudents() {
 export default async function Home() {
   const students = await getStudents();
 
-  const tableData = students.map((student) => ({
+  const tableData = students.map((student: typeof students[0]) => ({
     id: student.id,
     studentNumber: student.studentNumber,
     firstName: student.firstName,
